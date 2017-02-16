7cd23758761f5f2cef2ce50b8a6b25db2578dc14
Jason Pilbrough <09reebok@gmail.com>
2017-02-16 15:22:55 +0200

    add new custom CellEditor&Renderer classes

---
+++ src/com/jasonpilbrough/helper/SmartCellEditor.java
@@ -0,0 +1,54 @@
+package com.jasonpilbrough.helper;
+
+import java.awt.Color;
+import java.util.EventObject;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+public class SmartCellEditor extends DefaultCellEditor{
+	private static final Border red = new LineBorder(Color.red);
+    private static final Border black = new LineBorder(Color.black);
+    
+	private static final long serialVersionUID = 1L;
+	private Class thisClass;
+	private JTextField text;
+	public SmartCellEditor(JTextField textField, Class thisClass) {
+		super(textField);
+		this.text = textField;
+		this.thisClass = thisClass;
+	}
+	@Override
+	public boolean isCellEditable(EventObject anEvent) {
+		return super.isCellEditable(anEvent);
+	}
+	@Override
+	public boolean stopCellEditing() {
+		if(this.thisClass == Money.class){
+			try {
+				Money money = new Money(getCellEditorValue());
+			} catch (ClassCastException | NumberFormatException e) {
+				 text.setBorder(red);
+		         return false;
+			}
+		} else if(this.thisClass == Integer.class){
+			try {
+				Integer val = new Integer(getCellEditorValue().toString());
+			} catch (ClassCastException | NumberFormatException e) {
+				 text.setBorder(red);
+		         return false;
+			}
+		} else if(this.thisClass == DateInTime.class){
+			try {
+				DateInTime date = new DateInTime(getCellEditorValue().toString());
+				date.validate();
+			} catch (FailedValidationException e) {
+				 text.setBorder(red);
+		         return false;
+			}
+		}
+		return super.stopCellEditing();
+	}
+}
---
+++ src/com/jasonpilbrough/helper/SmartCellRenderer.java
@@ -0,0 +1,27 @@
+package com.jasonpilbrough.helper;
+
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class SmartCellRenderer extends DefaultTableCellRenderer{
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		
+		JLabel l = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		
+		if(table.getModel().getColumnClass(column)==Id.class){
+			setHorizontalAlignment(JLabel.LEFT);
+		} else if(table.getModel().getColumnClass(column)==Money.class){
+			setHorizontalAlignment(JLabel.RIGHT);
+			l.setText(new Money(value).toStringWithoutCurrency());
+		} else if(table.getModel().getColumnClass(column)==Integer.class){
+			setHorizontalAlignment(JLabel.RIGHT);
+		}
+		
+		return l;
+	}
+}
