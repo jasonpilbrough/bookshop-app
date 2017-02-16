bb07c5c1831173c6a53fd2b09d92eea554cd82da
Jason Pilbrough <09reebok@gmail.com>
2017-02-16 14:12:46 +0200

    cause member validation to happen first in AddMemberController

--- src/com/jasonpilbrough/vcontroller/AddMemberController.java
+++ src/com/jasonpilbrough/vcontroller/AddMemberController.java
@@ -35,44 +35,43 @@ public class AddMemberController extends Controller {
 	public void actionPerformed(ActionEvent e) {
 		switch(e.getActionCommand()){
 		case "confirm":
-			if(Boolean.parseBoolean(view.getFields().get("defered").toString())){
-				try {
+			try {
+				model.validMember(view.getFields().get("name").toString(), view.getFields().get("phone_number").toString());
+				if(Boolean.parseBoolean(view.getFields().get("defered").toString())){
+					
 					model.addMember(view.getFields().get("name").toString(), view.getFields().get("phone_number").toString(),false);
 					parent.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "init"));	
 					Drawable dialog4 = viewHandler.makeMessageDialog("Member successfully added","Success",this);
-					dialog4.draw();
-				} catch (FailedValidationException e2) {
-					Drawable dialog4 = viewHandler.makeMessageDialog(e2.getMessage(),"Error",this);
-					dialog4.draw();
-				}
-				
-			}else{
-				viewHandler.displayView("AddIncidental", "MEMBERSHIP FEE",model.getMembershipFee(),new Controller() {
-					
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						switch(e.getActionCommand()){
-						case "confirm":
-							try {
-								model.addMember(view.getFields().get("name").toString(), view.getFields().get("phone_number").toString(),true);
-								parent.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "init"));	
-								//dialog.setVisible(false);
-								Drawable dialog4 = viewHandler.makeMessageDialog("Member successfully added","Success",this);
-								dialog4.draw();
-							} catch (FailedValidationException e2) {
-								Drawable dialog4 = viewHandler.makeMessageDialog(e2.getMessage(),"Error",this);
-								dialog4.draw();
-							}
-							break;
-						default:
-							throw new RuntimeException("Command "+e.getActionCommand()+" not registered with controller");
-						}
+					dialog4.draw();	
+				}else{
+					viewHandler.displayView("AddIncidental", "MEMBERSHIP FEE",model.getMembershipFee(),new Controller() {
 						
-					}
-				});
-			   
-				
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							switch(e.getActionCommand()){
+								case "confirm":
+									try {
+										model.addMember(view.getFields().get("name").toString(), view.getFields().get("phone_number").toString(),true);
+										parent.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "init"));	
+										//dialog.setVisible(false);
+										Drawable dialog4 = viewHandler.makeMessageDialog("Member successfully added","Success",this);
+										dialog4.draw();
+									} catch (FailedValidationException e2) {
+										Drawable dialog4 = viewHandler.makeMessageDialog(e2.getMessage(),"Error",this);
+										dialog4.draw();
+									}
+									break;
+								default:
+									throw new RuntimeException("Command "+e.getActionCommand()+" not registered with controller");
+							}
+						}
+					});	
+				}
+			} catch (FailedValidationException e2) {
+				Drawable dialog4 = viewHandler.makeMessageDialog(e2.getMessage(),"Error",this);
+				dialog4.draw();
 			}
+			
 			
 			break;
 			
